a4c6d9f26859dd23b79436e29670ab3ee07bf882
neydermarquez <neydermarquezobregon@gmail.com>
2026-07-30 17:23:24 -0500

    Actualizar versión de la aplicación a v1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.soren.airpodscompanion"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         if (useHiddenApiNativeBridge) {
             externalNativeBuild {
